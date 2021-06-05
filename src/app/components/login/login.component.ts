b40fbbd44a01a1b45fa375dd678c0e1a1a467b50
Jugal Singh <jks2127@gmail.com>
2021-06-05 12:07:45 +0530

    enter

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  credentials={
+    username:"",
+    password:""
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    console.log("form submited");
+   window.alert("clicked submit"); 
   }
 
 }
